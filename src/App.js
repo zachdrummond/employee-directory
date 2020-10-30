3c49dad0655225fd2cd6d32001ce9f314a8d86b3
Zach Drummond <zachdrummond3@gmail.com>
2020-10-30 17:50:48 -0400

    Added EmployeeTable to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import EmployeeTable from "./components/EmployeeTable";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <EmployeeTable/>
     </>
   );
 }
