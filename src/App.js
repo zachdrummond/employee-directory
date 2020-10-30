33ecaa22d9469ffe48162002afbe0f1398ab7d30
Zach Drummond <zachdrummond3@gmail.com>
2020-10-30 17:47:39 -0400

    Added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className = "">
-      
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="text-center mt-5 mb-5">
+      <h1>Employee Directory</h1>
+      <p>
+        Click on headers to filter by heading or use the search box to narrow
+        your results.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
