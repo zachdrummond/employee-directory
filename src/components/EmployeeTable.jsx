459fdb79b2b7f4a41b5a70808327d93cc016a05b
Zach Drummond <zachdrummond3@gmail.com>
2020-10-30 17:51:06 -0400

    Added table to employeeTable

--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -7,8 +7,40 @@ class EmployeeTable extends Component {
 
   render() {
     return (
-      <>
-      </>
+      <table className= "table striped bordered hover">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
